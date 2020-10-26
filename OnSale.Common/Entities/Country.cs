84d701b42a2476a8e4fc7aa2a7c8809cd7c060c8
JavAgudel0 <jeagudelo76@misena.edu.co>
2020-10-25 21:01:53 -0500

    Creacion de crud y arreglos en las views

---
+++ OnSale.Common/Entities/Country.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnSale.Common.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The filed {0} must contain less than {1} character")]
+        [Required]
+        public string Name { get; set; }
+    }
+}
